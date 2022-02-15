72569152a9962e0329a429473d863fbe6168e7dc
Leo Dion <leogdion@brightdigit.com>
2022-02-15 18:44:42 -0500

    [skip ci]

--- Package.swift
+++ Package.swift
@@ -15,18 +15,14 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/shibapm/Komondor", from: "1.1.0"), // dev
-    // .package(url: "https://github.com/eneko/SourceDocs", from: "1.2.1"), // dev
     .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
     .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
     .package(url: "https://github.com/shibapm/Rocket.git", from: "1.2.0"), // dev
-        .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0"), // dev
     .package(path: "../Spinetail"),
     .package(path: "../PrchVapor")
   ],
   targets: [
     .target(name: "SpinetailVapor", dependencies: ["Spinetail", "PrchVapor"]),
-    .target(name: "SpinetailVaporApp", dependencies: ["SpinetailVapor"]),
-    .target(name: "SpinetailNIOApp", dependencies: ["PrchVapor", "Spinetail"])
   ]
 )
 
@@ -50,15 +46,7 @@ let package = Package(
       ]
     ],
     "komondor": [
-      "pre-push": [
-        // swiftlint:disable:next line_length
-        // "swift test --enable-code-coverage --enable-test-discovery",
-        // swiftlint:disable:next line_length
-        // "swift run swift-test-codecov .build/debug/codecov/PrchNIO.json --minimum \(requiredCoverage)"
-      ],
       "pre-commit": [
-        // swiftlint:disable:next line_length
-        // "swift test --enable-code-coverage --enable-test-discovery --generate-linuxmain",
         "swift run swiftformat .",
         "swift run swiftlint autocorrect",
         // swiftlint:disable:next line_length
