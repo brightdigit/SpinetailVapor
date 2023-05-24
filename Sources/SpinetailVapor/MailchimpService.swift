13e7a52341278ea67767dab1f5dfcac4e0c9a342
Leo Dion <leogdion@brightdigit.com>
2023-05-24 16:47:54 -0400

    finished upgrade

---
+++ Sources/SpinetailVapor/MailchimpService.swift
@@ -0,0 +1,29 @@
+import Prch
+import PrchVapor
+import Spinetail
+import Vapor
+
+public protocol MailchimpService : ServiceProtocol where ServiceAPI == SpinetailAPI {
+  
+}
+
+class MailchimpServiceImpl : MailchimpService, Service {
+
+
+  
+  internal init(
+    session: SessionClient,
+    api: SpinetailAPI,
+    authorizationManager: NullAuthorizationManager<SessionType.AuthorizationType> = NullAuthorizationManager<SessionType.AuthorizationType>()
+  ) {
+    self.session = session
+    self.api = api
+    self.authorizationManager = authorizationManager
+  }
+  
+
+  let session: SessionClient
+  let api: SpinetailAPI
+  let authorizationManager: any SessionAuthenticationManager
+  
+}
